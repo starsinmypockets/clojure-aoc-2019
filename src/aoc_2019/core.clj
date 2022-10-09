a88c3c75bacd14d53bc195e15076e2624c358a76
Paul Walker <starsinmypockets@gmail.com>
2022-10-08 22:02:20 -0400

    Day 4 -- part 2

--- src/aoc_2019/core.clj
+++ src/aoc_2019/core.clj
@@ -131,26 +131,54 @@
 ;;
 
 ; SO
-(defn digits [number] (map #(Character/digit % 10) (str number)))
+(defn digits [number] (vec (map #(Character/digit % 10) (str number))))
 ; (defn has-twin [ll] true)
 
-(defn asc [ll] 
+(defn asc? [ll] 
   (let [asc-vals (map-indexed (fn [idx _itm] (if (< idx (- (count ll) 1)) (>= (nth ll (+ idx 1)) (nth ll idx)))) ll)]
     (= (count (filter true? asc-vals)) 5)))
 
-(defn has-pair [ll] 
+(defn has-pair? [ll] 
   (let [asc-vals (map-indexed (fn [idx _itm] (if (< idx (- (count ll) 1)) (= (nth ll (+ idx 1)) (nth ll idx)))) ll)]
     (>= (count (filter true? asc-vals)) 1)))
 
 (defn valid? [n]
   (let [ll (digits n)]
-   (and (asc ll) (has-pair ll))))
+   (and (asc? ll) (has-pair? ll))))
 
 (defn crack-pass [low hi]
   (let [ xs (filter valid? (range low hi))]
     (count xs)))
 
 (defn day-4 [] (crack-pass 357253 892942))
+
+;;
+; Day 4 -- Part 2
+;;
+
+(defn xpair? [ll i]
+  (if (>= i 5)
+    false
+    (if (= i 4)
+      (if (= (ll 0) (ll 1))
+        true
+        false)
+    (if (= (ll 0) (ll 1))
+      (let [n (count (take-while #(= (ll 0) %) ll))]
+        (if (= n 2)
+          true 
+          (xpair? (vec (drop n ll)) (+ i n))))
+      (xpair? (vec (drop 1 ll)) (+ i 1))))))
+
+(defn valid-2? [n]
+  (let [ll (digits n)]
+   (and (asc? ll) (xpair? ll 0))))
+
+(defn crack-pass-2 [low hi]
+  (let [ xs (filter valid-2? (range low hi))]
+    (count xs)))
+
+(defn day-4-part-2 [] (crack-pass-2 357253 892942))
 
 (defn -main
   "Advent of Code -- 2019 https://adventofcode.com/2019/"
@@ -161,4 +189,5 @@
   (day-3)
   (day-3-part-2)
   (day-4)
+  (day-4-part-2)
 )
--- test/aoc_2019/core_test.clj
+++ test/aoc_2019/core_test.clj
@@ -34,3 +34,15 @@
     (and
       (is (false? (aoc/valid? 12321))
       (is (true? (aoc/valid? 122345)))))))
+
+(deftest test-day-4-2  (testing "Day 4 -- part 2"
+    (and
+      (is (true?  (aoc/xpair? [1 1 1 4 5 5] 0)))
+      (is (true?  (aoc/xpair? [1 1 2 4 5 6] 0)))
+      (is (true?  (aoc/xpair? [1 1 2 2 3 3] 0)))
+      (is (false? (aoc/xpair? [1 2 4 5 6 7] 0)))
+      (is (false? (aoc/xpair? [1 2 4 4 4 6] 0)))
+      (is (false? (aoc/xpair? [1 2 3 4 4 4] 0)))
+      (is (false? (aoc/xpair? [3 5 7 7 7 7] 0)))
+    )))
+
